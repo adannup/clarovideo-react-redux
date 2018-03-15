b659bf3d4da159a286fea346435c87fb2cc1bfad
adannup <zad3life@gmail.com>
2018-03-15 17:11:07 -0600

    Fix problem with state and search Filter

--- src/js/Item/ItemContainer.js
+++ src/js/Item/ItemContainer.js
@@ -1,10 +1,12 @@
 import { connect } from 'react-redux';
 import { onClickItemDetail, requestPost } from './ItemAction';
+import setFilterQuery from '../Filter/FilterSearchAction';
 import Item from './Item';
 
 const mapDispatchToProps = (dispatch, ownProps) => ({
   onClickItemDetail: () => {
     dispatch(requestPost());
+    dispatch(setFilterQuery(''));
     dispatch(onClickItemDetail(ownProps.id));
   },
 });
